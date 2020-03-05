499879b72e4cbda37eb798f23499873d1fda6c77
Adelina09 <adelina.bronda@academytrainee.com>
2020-03-05 15:23:19 +0000

    first attempt

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ mysql=MySQL(app)
 
 app.config['MYSQL_HOST']='34.89.88.117'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='EEniemEEnie1'
+app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='SFIA1'
 
 @app.route('/')
